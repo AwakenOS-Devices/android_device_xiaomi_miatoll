c514f7b509228668673a54dc169ff21d166475da
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 00:27:07 +0200

    miatoll: Add power AIDL DT2W support via sysfs node
    * Also drop power AIDL extension
    
    Change-Id: Idcff758444a5c89ba20f2ae804a407ee78c445cb
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := atoll
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
