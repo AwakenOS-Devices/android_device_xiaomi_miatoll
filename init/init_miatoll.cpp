b11b31c9968be97937c6af5e4d822e9ae9c65f96
Jyotiraditya Panda <jyotiraditya@aospa.co>
2022-02-08 02:24:26 +0600

    miatoll: init: Don't override fingeprint and desc
    Change-Id: I27fff7a9d8bbecb41849ba7c2b8d09087ab086c3
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- init/init_miatoll.cpp
+++ init/init_miatoll.cpp
@@ -65,10 +65,6 @@ void set_device_props(const std::string brand, const std::string device, const s
 }
 
 void load_device_properties() {
-    property_override("ro.build.fingerprint",
-                      "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
-    property_override("ro.build.description",
-                      "raven-user 12 SQ1D.220105.007 8030436 release-keys");
     std::string hwname = GetProperty("ro.boot.hwname", "");
     std::string region = GetProperty("ro.boot.hwc", "");
     std::string hwversion = GetProperty("ro.boot.hwversion", "");
