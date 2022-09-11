b1849de59afa60056d25d8b88e087bf619512bf1
Bowgo Tsai <bowgotsai@google.com>
2022-09-11 20:03:25 +0600

    miatoll: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    
    Change-Id: I0cf78bfc80c24bb217bb88831faa5a29ba04d194

--- device.mk
+++ device.mk
@@ -232,6 +232,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor
 
+# GSI
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
