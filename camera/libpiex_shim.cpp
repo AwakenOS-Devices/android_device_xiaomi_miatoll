16fbf23aa42b777b4a972d8f822ecfe1aceafb2d
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-09-12 16:56:55 +0600

    miatoll: camera: Format code with ``clang-format``
     * Also add AOSP's SPDX identifiers
    
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- camera/libpiex_shim.cpp
+++ camera/libpiex_shim.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,9 +9,8 @@
 
 namespace piex {
 
-Error GetPreviewImageData(StreamInterface* data,
-                          PreviewImageData* preview_image_data) {
+Error GetPreviewImageData(StreamInterface *data, PreviewImageData *preview_image_data) {
     return GetPreviewImageData(data, preview_image_data, nullptr);
 }
 
-} // namespace piex
+}  // namespace piex
