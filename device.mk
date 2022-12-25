5dd4aefa974e7a9e203057feb7eb25dcd07c3c7b
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-12-25 13:03:40 +0600

    miatoll: Rework bluetooth properties
     * move to vendor properties as most migrated to sysprops in Android 13

--- device.mk
+++ device.mk
@@ -68,24 +68,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    bluetooth.device.class_of_device=90,2,12 \
-    bluetooth.profile.a2dp.source.enabled=true \
-    bluetooth.profile.asha.central.enabled=true \
-    bluetooth.profile.avrcp.target.enabled=true \
-    bluetooth.profile.bas.client.enabled=true \
-    bluetooth.profile.gatt.enabled=true \
-    bluetooth.profile.hfp.ag.enabled=true \
-    bluetooth.profile.hid.device.enabled=true \
-    bluetooth.profile.hid.host.enabled=true \
-    bluetooth.profile.map.server.enabled=true \
-    bluetooth.profile.opp.enabled=true \
-    bluetooth.profile.pan.nap.enabled=true \
-    bluetooth.profile.pan.panu.enabled=true \
-    bluetooth.profile.pbap.server.enabled=true \
-    bluetooth.profile.sap.server.enabled=true \
-    persist.vendor.btstack.enable.lpa=true
-
 PRODUCT_VENDOR_PROPERTIES += \
     persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac-lc3 \
     persist.bluetooth.a2dp_offload.disabled=false \
